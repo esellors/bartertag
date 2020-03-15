208cd0b19c3af256e6697edb1c01b60b2df2e254
Eric Sellors <github@esellors.com>
2020-03-14 21:27:24 -0500

    updated condition for esellors link

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App(props) {
 
   return (
     <>
+      { document.referrer === 'https://www.esellors.com/' ? <Referrer /> : null }
       <div id='routes_top_container'>
-      { document.referrer !== 'https://www.esellors.com/' ? <Referrer /> : null }
         <Header />
         <main>
           {
